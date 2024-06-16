d9e8e01789031e3a8b80a070b8e30ca0da1865fa
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-06-16 20:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function toCase(text) {
   // write your code here
+	if(text==="")
+	{
+		return "-";
+	}
+	let lower=text.toLowerCase();
+	let upper=text.toUpperCase();
+	return lower+"-"+upper;
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
+
 alert(toCase(text));
